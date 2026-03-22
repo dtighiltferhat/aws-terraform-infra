e39448858f98c0a3189beebbc2dc5f5e1f113cf8
Djamal Tighilt Ferhat <dtighiltferhat@gmail.com>
2026-03-21 21:30:07 -0400

    feat(vpc): add VPC module public/private subnets, NAT gateway, and rout tables

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,33 @@
+# modules/vpc/variables.tf
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "common_tags" {
+  description = "Common tags applied to all resources"
+  type        = map(string)
+  default     = {}
+}
